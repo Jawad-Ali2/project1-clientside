b9a5edecbde69b7a43f569d1266b06f00ed18e53
Jawad Ali <jawadali66667@gmail.com>
2023-06-15 15:05:51 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,116 @@
+// Applying client side render to deal with the page scroll up glitch
+'use client';
+import { useEffect, useState } from 'react';
+
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
+import { fetchCars } from '@/utils'
+import Image from 'next/image';
+
+export default function Home() {
+
+  // Uncomment if want to apply server side render
+  // const allCars = await fetchCars({
+  //   manufacturer: searchParams.manufacturer || '',
+  //   year: searchParams.year || 2022,
+  //   fuel: searchParams.fuel || '',
+  //   limit: searchParams.limit || 10,
+  //   model: searchParams.model || '',
+  // });
+
+  const [allCars, setAllCars] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Search states
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  // Filter States
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
+
+  // Pagination state
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+    setLoading(true);
+
+    try {
+      const results = await fetchCars({
+        manufacturer: manufacturer || '',
+        year: year || 2022,
+        fuel: fuel || '',
+        limit: limit || 10,
+        model: model || '',
+      })
+      setAllCars(results);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getCars();
+
+  }, [fuel, year, limit, manufacturer, model])
+
+
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id='discover'>
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
+
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} setFilter={setFuel} />
+            <CustomFilter title="year" options={yearsOfProduction} setFilter={setYear} />
+          </div>
+        </div>
+
+        {allCars.length > 0 ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => <CarCard car={car} />)}
+            </div>
+
+            {loading && (
+              <div className='mt-16 w-full flex-center'>
+                <Image
+                  src="/loading.svg"
+                  alt='loader'
+                  width={50}
+                  height={50}
+                  className='object-contain'
+                />
+              </div>
+            )}
+
+            <ShowMore
+              pageNumber={limit / 10}
+              isNext={limit > allCars.length}
+              setLimit = {setLimit}
+
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops! no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
+    </main>
+  )
+}
